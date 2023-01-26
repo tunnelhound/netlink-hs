d8b2ad29b82a8be98c119acdb7929707610e69bd
Travis Athougies <travis@athougies.net>
2023-01-26 23:57:15 +0000

    Update flake

---
+++ nix/hix.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: {
+  name = "netlink-hs";
+  compiler-nix-name = "ghc944"; # Version of GHC to use
+
+  crossPlatforms = p: [];
+  #crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
+  #  p.mingwW64
+  #  p.ghcjs
+  #] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+  #  p.musl64
+  #]);
+
+  # Tools to include in the development shell
+  shell.tools.cabal = "latest";
+  # shell.tools.hlint = "latest";
+  # shell.tools.haskell-language-server = "latest";
+}
